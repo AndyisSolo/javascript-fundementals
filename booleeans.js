b284cc030437727274c27d23354da32c0ea23af2
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-10-27 11:16:53 +0100

    completed sixth tutorial on js

---
+++ booleeans.js
@@ -0,0 +1,11 @@
+let truevalue = true;
+console.log(trueValue);
+let falseValue =false;
+console.log(falsevalue)
+
+
+
+let notTrue = !true;
+console.log(notTrue);
+let notFalse = !(false);
+console.log(notFalse);
