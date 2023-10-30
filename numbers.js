f5e7ead262f3d0427ee862e10a1ed6147e9c3175
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-10-30 18:55:11 +0000

    done numbers.js

---
+++ numbers.js
@@ -0,0 +1,11 @@
+let num1 = 6;
+let numb2 = -10000;
+let num3 = 1.234567;
+
+
+let numb4 = 1560000000000000000000;
+console.log(numb4)
+let num5 = -0.000000000000000000000000021;
+console.log (num5)
+
+let num6 = 1.2e+10;
