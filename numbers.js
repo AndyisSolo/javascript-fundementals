c6152d0a60d051f3063b99bbf2faf6b81d1b9fb3
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-11-23 19:09:19 +0000

    completed numbers.js

--- numbers.js
+++ numbers.js
@@ -9,3 +9,9 @@ let num5 = -0.000000000000000000000000021;
 console.log (num5)
 
 let num6 = 1.2e+10;
+console.log(num6);
+
+console.log(`Minimum  Value: ${Number.MIN_VALUE}`);
+console.log(`Minimum Safe Integer: ${Number.Min_SAFE_INTEGER}`);
+console.log(`Maximum value; ${Number.MAX_VALUE}`);
+console.log(`Maximum SafeInteger: ${Number.MAX_SAFE_INTEGER}`);
