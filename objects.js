e2b19e0f9660dda915df42d66c4db1cc60c18752
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-12-18 13:56:01 +0000

    Created and learnt how JavaScript objects work

--- objects.js
+++ objects.js
@@ -5,4 +5,34 @@ let obj2 = new Object();
 obj1.firstName = "Davud";
 obj1.LastName = "Tucker";
 obj1.isActive = true;
-obj1.startDate= new Date("January 1, 2023");
+obj1.startDate = new Date("January 1, 2022");
+obj.vacationDays = 14;
+console.log(obj1);
+
+
+let obj3 = {
+    firstName: "David",
+    lastName: "Tucker",
+    isActive: true,
+    startdate: new date ("January 1, 2022"),
+    vacationDays: 14
+};
+console.log(obj3);
+
+console.log(`First Name: ${obj3.firstName}`);
+console.log(`Last Name: ${obj3["lastName"]}`);
+
+
+
+delete obj3.firstName
+console.log(obj3);
+
+
+
+console.log(`Non-existent value; ${obj3.middleName}`);
+
+
+
+let obj4=obj3;
+ obj4.lastName ="Smith";
+ console.log(`Last Name (obj3) : ${obj3.lastName}`);
