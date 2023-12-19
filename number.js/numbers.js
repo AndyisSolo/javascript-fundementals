35b756b31b6b5a5458cb28ea9d05b46ece4c02a0
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-12-19 16:08:44 +0000

    Fixed problems

--- number.js/numbers.js
+++ number.js/numbers.js
@@ -13,23 +13,24 @@ console.log(`Greece: ${num2.toLocaleString('el-El')}`);
 console.log(`Bangladesh: ${num2.toLocaleString(`bg-bg`)}`);
 console.log(`Punjab: ${num2.toLocaleString('pa-PA')}`);
 
+let formatter1 = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
+
 let Salary = 10000;
 let monthlySalary = Salary / 12;
-console.log(`Monthly Salary: ${MonthlySalary.toFixed(2)}`);
+console.log(`Monthly Salary: ${monthlySalary.toFixed(2)}`);
 console.log(`US Dollars: ${formatter1.format(monthlySalary)}`);
 
 
-let formatter1 = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'});
 console.log(`Us Dollars: ${formatter1.format(monthlySalary)}`);
 
-let formatter2 = new Intl.NumberFormat('de-DE', { style: 'currrency', currency: 'Eur'})
+let formatter2 = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR'})
 console.log(`Euros: ${formatter2.format(monthlySalary)}`);
 
-let formatter3= new Intl.NumberFormat('ja-JA', { style: 'currrency', currency: 'JPY'})
+let formatter3= new Intl.NumberFormat('ja-JA', { style: 'currency', currency: 'JPY'})
 console.log(`Yen: ${formatter3.format(monthlySalary)}`)
 
 
-let formatter4= new Intl.NumberFormat('zh-HK', { style: 'currrency', currency: 'HKD'})
+let formatter4= new Intl.NumberFormat('zh-HK', { style: 'currency', currency: 'HKD'})
 console.log(`Hong Kong Dollars: ${formatter4.format(monthlySalary)}`);
 
 
