2c4d210b9265b4421423b58181fc4a50d3b47948
Andrew Solopovas <andrew.solopovas@gmail.com>
2023-12-19 13:20:21 +0000

    learnt how toconvernt boolean to strings and how to convert strings to boolean

---
+++ conversion.js
@@ -0,0 +1,45 @@
+let SrtValue = "Hi";
+console.log(typeof(SrtValue))
+let numValue = 1;
+console.log(typeof(numValue));
+let boolValue = true;
+console.log(typeof(booValue));
+
+
+let age = 41;
+let firstName = "David";
+let description = `${firstname} is ${age} years old`;
+console.log(description);
+
+
+let ageString = "41";
+let ageNum = Number(ageString);
+console.log(typeof (ageNum));
+let ageNewNum = new Number(ageString);
+console.log(typeof(ageNewNumb));
+
+let ageString2 = "forty-one";
+let ageNum2 = Number(ageString2);
+console.log(`Age String 2 : ${ageNum2}`);
+let isInvalid = isNaN(ageNum2);
+console.log(`Is Invalid; ${isInvalid}`);
+
+let num1 = 1;
+let Num2 = 0;
+let bool1 = Boolean(num1);
+let bool2 = Boolean(Num2);
+console.log(`Num1: ${bool1}) Num2: ${bool2}`);
+
+
+let str1 = "Hello";
+let bool3 = Boolean(str1);
+let vali;
+let bool4 = Boolean(vali);
+console.log(`Bool 3; ${bool3} Bool4; ${bool4}`);
+
+
+let bool5 = true;
+console.log(`Bool 5: ${bool5}`);
+
+
+
